8af7cd56bf4dcca4cf9821cedfd815696eb23618
PavelPshen <pshen-leti@mail.ru>
2024-03-16 12:42:44 +0300

    3.3. Фиксация изменений на github [#243564 #485957]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -1,11 +1,8 @@
 package ru.job4j;
 
-public class StudentInfo
-{
-    public static void main(String[] args)
-    {
+public class StudentInfo {
+    public static void main(String[] args) {
         System.out.println("Pavel Pshenitsyn");
         System.out.println("26.11.1989");
-
     }
 }
