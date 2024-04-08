0dac0af9c708accf910a46db93c7146fb0b54ecb
PavelPshen <pshen-leti@mail.ru>
2024-04-08 07:22:12 +0300

    2.0. Рефакторинг - Расстояние между точками. [#122587]

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -1,19 +1,34 @@
 package ru.job4j.condition;
 
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
+
 public class Point {
-    public static double distance(int x1, int y1, int x2, int y2) {
-        double result = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-        return result;
+
+    private  int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distance(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
     }
 
     public static void main(String[] args) {
-        double result = Point.distance(0, 0, 2, 0);
-        System.out.println("result (0, 0) to (2, 0) " + result);
-        result = Point.distance(1, 1, 5, 5);
-        System.out.println("result 1, 1) to (5, 5) " + result);
-        result = Point.distance(2, 4, 6, 12);
-        System.out.println("result (2, 4) to (6, 12) " + result);
-        result = Point.distance(4, 2, 6, 12);
-        System.out.println("result (4, 2) to (6, 12) " + result);
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        Point c = new Point(1, 1);
+        Point d = new Point(5, 5);
+        Point e = new Point(2, 4);
+        Point f = new Point(6, 12);
+        Point g = new Point(4, 2);
+        double distance = a.distance(b);
+        System.out.println(distance);
+        System.out.println(c.distance(d));
+        System.out.println(e.distance(f));
+        System.out.println(g.distance(f));
     }
 }
--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -6,34 +6,28 @@ import static org.assertj.core.api.Assertions.*;
 class PointTest {
     @Test
     void when00to20then2() {
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
         double expected = 2;
-        double output = Point.distance(x1, y1, x2, y2);
+        double output = a.distance(b);
         assertThat(output).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
     void whenMinus1Minus1to11then2dot82() {
-        int x1 = -1;
-        int y1 = -1;
-        int x2 = 1;
-        int y2 = 1;
+        Point a = new Point(-1, -1);
+        Point b = new Point(1, 1);
         double expected = 2.82;
-        double output = Point.distance(x1, y1, x2, y2);
+        double output = a.distance(b);
         assertThat(output).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
     void whenMinus22to22then4() {
-        int x1 = -2;
-        int y1 = 2;
-        int x2 = 2;
-        int y2 = 2;
+        Point a = new Point(-2, 2);
+        Point b = new Point(2, 2);
         double expected = 4;
-        double output = Point.distance(x1, y1, x2, y2);
+        double output = a.distance(b);
         assertThat(output).isEqualTo(expected, withPrecision(0.01));
     }
 }
